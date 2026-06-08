dfae06cd529434f69c0bebbc9fc26d69b8eda183
lili <lili.sepulveda.96@gmail.com>
2026-06-08 18:41:29 +0200

    chore(frontend): wire dataProvider to admin and add temporary ListGuesser for testing

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
-import { Admin, Resource } from "react-admin";
+import { Admin, Resource, ListGuesser } from "react-admin";
 import "./App.css";
 import { authProvider } from "./providers/authProvider";
+import { dataProvider } from "./providers/dataProvider";
 
 function App() {
   return (
-    <Admin authProvider={authProvider}>
-      <Resource name="test" />
+    <Admin authProvider={authProvider} dataProvider={dataProvider}>
+      <Resource name="customers" list={ListGuesser} />
+      <Resource name="projects" list={ListGuesser} />
     </Admin>
   );
 }

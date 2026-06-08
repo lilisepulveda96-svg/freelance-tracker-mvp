ce4b2322f215530a8ae5db3dcfa9b782c536f8a2
lili <lili.sepulveda.96@gmail.com>
2026-06-09 00:55:10 +0200

    chore(frontend): register customers and projects resources in Admin

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,29 @@
-import { Admin, Resource, ListGuesser } from "react-admin";
+import { Admin, Resource } from "react-admin";
 import "./App.css";
 import { authProvider } from "./providers/authProvider";
 import { dataProvider } from "./providers/dataProvider";
+import { CustomerList } from "./resources/customers/CustomerList";
+import { CustomerCreate } from "./resources/customers/CustomerCreate";
+import { CustomerEdit } from "./resources/customers/CustomerEdit";
+import { ProjectList } from "./resources/projects/ProjectList";
+import { ProjectCreate } from "./resources/projects/ProjectCreate";
+import { ProjectEdit } from "./resources/projects/ProjectEdit";
 
 function App() {
   return (
     <Admin authProvider={authProvider} dataProvider={dataProvider}>
-      <Resource name="customers" list={ListGuesser} />
-      <Resource name="projects" list={ListGuesser} />
+      <Resource
+        name="customers"
+        list={CustomerList}
+        create={CustomerCreate}
+        edit={CustomerEdit}
+      />
+      <Resource
+        name="projects"
+        list={ProjectList}
+        create={ProjectCreate}
+        edit={ProjectEdit}
+      />
     </Admin>
   );
 }
